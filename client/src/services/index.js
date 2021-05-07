9689e530468615a564f0791606f13ed146e3a5ef
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-05-07 16:47:49 -0500

    added process.env

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,7 +1,7 @@
-export const baseURL = `https://api.airtable.com/v0/${REACT_APP_AIRTABLE_BASE}/theshoppies`;
+export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/theshoppies`;
 
 export const config = {
   headers: {
-    Authorization: `Bearer ${REACT_APP_AIRTABLE_KEY}`,
+    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
   },
 };
